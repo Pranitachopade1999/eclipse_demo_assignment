53f1ae9ead97d69b045e05ed7ea40f2517e54e59
Pranita <PRANITA.LAHU-CHOPADE@CAPGEMINI.COM>
2022-01-20 16:58:02 +0530

    Addition Program

---
+++ SampleProject/src/Addition.java
@@ -0,0 +1,11 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=20,b=30;
+		System.out.println("Addition : " +(a+b));
+	}
+
+}
